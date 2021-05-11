1d55da35478306e1ec1ff91d9131fca81a36eb18
Kishan Gupta <48645362+KishanGupta114@users.noreply.github.com>
2021-05-11 16:27:23 +0530

    Updates

---
+++ precache-manifest.dfd435fa07c44ec3a0cd3d76f669b389.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a28f1b60150434cc42239fe71fc66b",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "7a796601072e0e824c9b",
+    "url": "/Website/static/js/2.b89223c1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Website/static/js/2.b89223c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c86c9383a5787ee891e",
+    "url": "/Website/static/js/main.029569c2.chunk.js"
+  },
+  {
+    "revision": "c15716b5999deccda870",
+    "url": "/Website/static/js/runtime-main.5d75c918.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/Website/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/Website/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/Website/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/Website/static/media/svg-3.397c455a.svg"
+  }
+]);
